48660b6a9ed00bd45560a4e89f255a2beaba8774
Kenny <kennyazodo47@gmail.com>
2025-06-16 14:28:41 +0100

    working commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template('About.html')  # Replace with `render_template("about.html")` if you create one
+
+
+@app.route("/contact")
+def contact():
+    return render_template('Contact.html')
+
+
+@app.route("/startup")
+def startup():
+    return render_template('Startup.html')
+
+@app.route("/team")
+def team():
+    return render_template('Team.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
