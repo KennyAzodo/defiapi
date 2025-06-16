1ddfc36cbecf6fbc3cf8ac869500feae291dc93a
Kenny <kennyazodo47@gmail.com>
2025-06-16 15:43:45 +0100

    working commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -27,4 +29,5 @@ def team():
     return render_template('Team.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
